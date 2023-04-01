9f07ef723f47ee47e35711d57c3396fdf2cb0bda
KevinnaBintang <129611487+KevinnaBintang@users.noreply.github.com>
2023-04-01 23:29:15 +0700

    Add files via upload

---
+++ KEVINNA TUGAS2.php
@@ -0,0 +1,38 @@
+<!-- nama : Kevinna Bintang Maulidia
+nim : G.231.21.0188 -->
+
+<?php
+interface Hewan {
+    
+    function berjalan();
+    function makan();
+    function bersuara();
+}
+
+interface Monyet extends Hewan {
+    function berkendara();
+}
+
+class Dilatih implements Monyet {
+    
+    public function __construct() {
+    }
+    
+    function berjalan(){
+        echo "Berjalan";
+    }
+    function makan() {
+        echo "Makan";
+    }
+    function bersuara() {
+        echo "Bersuara";
+    }
+    function berkendara() {
+        echo "Berkendara";
+    }
+}
+
+$latih = new Dilatih();
+$latih-> berjalan()
+
+?>
